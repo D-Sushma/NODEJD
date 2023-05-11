be96511c4b2a3f613e799debfb5a417b4903642a
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2023-05-11 19:17:44 +0530

    make insert API name=test for practice

--- crudAPI/app.js
+++ crudAPI/app.js
@@ -26,64 +26,101 @@ app.get('/', (req, res) => {
 
 /*** Get All Items ** @return response()*/
 app.get('/all', (req, res) => {
-    let sqlQuery = "SELECT * FROM employee";
+    let sqlQuery = "SELECT * FROM dbuser";
     let query = conn.query(sqlQuery, (err, results) => {
         if (err) throw err;
         console.log(results);
-        res.send(apiResponse(results));
+        res.send((results));
     });
 });
 
-/*** Get Single Item ** @return response() api/items/:id*/
+// /*** Get Single Item ** @return response() api/items/:id*/
 app.get('/single/item/:id', (req, res) => {
-    let sqlQuery = "SELECT * FROM employee WHERE id=" + req.params.id;
+    let sqlQuery = "SELECT * FROM dbuser WHERE id=" + req.params.id;
     // let sqlQuery = "SELECT * FROM employee";
     let query = conn.query(sqlQuery, (err, results) => {
         if (err) throw err;
-        res.send(apiResponse(results));
+        console.log(results);
+        res.send((results));
     });
 });
 
-/*** Create New Item *post* @return response() */
-app.get('/insert/items',(req, res) => {
-    // let data = {name: req.body.name, address: req.body.address};
+// /*** Create New Item *post* @return response() */
+// app.get('/insert/items',(req, res) => {
+//     // let data = {name: req.body.name, address: req.body.address};
     
-    // let sqlQuery = "INSERT INTO employee SET ?";
-    let sqlQuery = "INSERT INTO employee (id, name, address) VALUES (6, 'aarti','durg')";
+//     // let sqlQuery = "INSERT INTO employee SET ?";
+//     let sqlQuery = "INSERT INTO dbuser (name, contact, subject) VALUES ('aarti','45236987563','DBMS')";
     
-    let query = conn.query(sqlQuery, (err, results) => {
-      if(err) throw err;
-      res.send(apiResponse(results));
-    });
-  });
+//     let query = conn.query(sqlQuery, (err, results) => {
+//       if(err) throw err;
+//       res.send((results));
+//     });
+//   });
+app.post('/insert/new', function (req, res) {
+  
+  let sqlQuery = `INSERT INTO dbuser
+  
+      (name, contact, subject) VALUES (?, ?, ?);`;
 
-  /*** Update Item *put* @return response() */
-app.get('/update/items/:id',(req, res) => {
-    let sqlQuery = "UPDATE employee SET name='"+req.body.name+"', address='"+req.body.address+"' WHERE id="+req.params.id;
-    
-    let query = conn.query(sqlQuery, (err, results) => {
-      if(err) throw err;
-      res.send(apiResponse(results));
-    });
-  });
+  // Value to be inserted
+  let userName = "Pratik";
+  let userContact = 4587963254;
+  let userSubject = "English";
 
-  /*** Delete Item *delete* @return response() */
-app.get('/delete/items',(req, res) => {
-    // let sqlQuery = "DELETE FROM employee WHERE id="+req.params.id+"";
-    let sqlQuery = "DELETE FROM employee where id=null";
+  let query = conn.query(sqlQuery, [userName, 
+    userContact,userSubject], (err, rows) => {
+      if (err) throw err;
+      console.log("Row inserted with id = "
+          + rows.insertId);
+  });
+});
+  app.post('/test', function (req, res) {
+     let  name = req.body.name;
+       let contact = req.body.contact;
+        let subject =  req.body.subject;
+    let sqlQuery = `INSERT INTO dbuser (name, contact, subject) VALUES ('${name}','${contact}','${subject}')`;
+    let query = conn.query(sqlQuery, (err, results) => {
+            if(err) throw err;
+            res.send((results));
+      });
+  });
+     
+  // app.post('/users',(req,res)=>{
+  //   createPool.query('INSERT INTO users SET?', req.body,
+  //   (err,res)=>{
+  //     if(err) throw err;
+  //     res.status(201).send(`User added with ID: ${res.insertId}`);
+  //   });
+  // });
+
+//   /*** Update Item *put* @return response() */
+// app.get('/update/items/:id',(req, res) => {
+//     let sqlQuery = "UPDATE employee SET name='"+req.body.name+"', address='"+req.body.address+"' WHERE id="+req.params.id;
+    
+//     let query = conn.query(sqlQuery, (err, results) => {
+//       if(err) throw err;
+//       res.send(apiResponse(results));
+//     });
+//   });
+
+//   /*** Delete Item *delete* @return response() */
+// app.get('/delete/items',(req, res) => {
+//     // let sqlQuery = "DELETE FROM employee WHERE id="+req.params.id+"";
+//     let sqlQuery = "DELETE FROM employee where id=null";
       
-    let query = conn.query(sqlQuery, (err, results) => {
-      if(err) throw err;
-        res.send(apiResponse(results));
-    });
-  });
+//     let query = conn.query(sqlQuery, (err, results) => {
+//       if(err) throw err;
+//         res.send(apiResponse(results));
+//     });
+//   });
 
-/*** API Response ** @return response() */
-function apiResponse(results) {
-    return JSON.stringify({ "status": 200, "error": null, "response": results });
-}
+// /*** API Response ** @return response() */
+// function apiResponse(results) {
+//     return JSON.stringify({ "status": 200, "error": null, "response": results });
+// }
 
-/*---------------Server listening----------------------*/
-app.listen(3000, () => {
-    console.log('Server started on port 3000...');
+// /*---------------Server listening----------------------*/
+app.listen(5000, () => {
+    console.log('Server started on port 5000...');
 });
